86b145812f1f5c992590ab99cba45c60f1d47db5
DeXoteric <dexoteric@gmail.com>
2024-08-02 17:30:38 +0200

    update

--- cookie_autoclicker.py
+++ cookie_autoclicker.py
@@ -22,7 +22,6 @@ INTERVAL_INCREMENT: float = 1.01
 INITIAL_INTERVAL: float = 15
 PAUSE_KEY_COMBINATION: set = {keyboard.Key.cmd, keyboard.Key.pause}
 SAVE_KEY_COMBINATION: set = {keyboard.Key.cmd, keyboard.Key.scroll_lock}
-CLICK_KEY_COMBINATION: set = {keyboard.Key.cmd, keyboard.Key.print_screen}
 INTERVAL_CHANCE: float = 0.1
 INTERVAL_MULTIPLAYER: float = 5
 
@@ -58,7 +57,7 @@ class CookieAutoclicker:
 
         time.sleep(1)
 
-        self.options_btn = self.driver.find_element(By.CSS_SELECTOR, ".subButton")
+        self.options_btn = self.driver.find_element(By.CLASS_NAME, "subButton")
 
         self.driver.find_element(By.CSS_SELECTOR, ".cc_btn").click()
 
@@ -150,9 +149,9 @@ class CookieAutoclicker:
     def buy_upgrades(self):
         while True:
             try:
-                upgrades = self.driver.find_element(
-                    By.CSS_SELECTOR, "#upgrades"
-                ).find_elements(By.CSS_SELECTOR, ".crate.upgrade.enabled")
+                upgrades = self.driver.find_element(By.ID, "upgrades").find_elements(
+                    By.CSS_SELECTOR, ".crate.upgrade.enabled"
+                )
                 for upgrade in upgrades:
                     upgrade.click()
             except StaleElementReferenceException:
@@ -183,22 +182,18 @@ class CookieAutoclicker:
     def spawn_golden_cookie(self):
         while True:
             try:
-                mana_text = self.driver.find_element(
-                    By.CSS_SELECTOR, "#grimoireBarText"
-                )
+                mana_text = self.driver.find_element(By.ID, "grimoireBarText")
 
                 current_mana = int(mana_text.text.split("/")[0].strip())
                 max_mana = int(mana_text.text.split("/")[1].split(" ")[0].strip())
                 is_mana_full = current_mana == max_mana
 
                 mana_cost = int(
-                    self.driver.find_element(
-                        By.CSS_SELECTOR, "#grimoirePrice1"
-                    ).text.strip()
+                    self.driver.find_element(By.ID, "grimoirePrice1").text.strip()
                 )
 
                 if self.is_buff_active and is_mana_full and current_mana >= mana_cost:
-                    self.driver.find_element(By.CSS_SELECTOR, "#grimoireSpell1").click()
+                    self.driver.find_element(By.ID, "grimoireSpell1").click()
             except StaleElementReferenceException:
                 continue
             except NoSuchElementException:
@@ -214,7 +209,7 @@ class CookieAutoclicker:
                     By.CSS_SELECTOR, ".framed.note.haspic.hasdesc"
                 )
                 for note in notes:
-                    close_button = note.find_element(By.CSS_SELECTOR, ".close")
+                    close_button = note.find_element(By.CLASS_NAME, "close")
                     close_button.click()
             except StaleElementReferenceException:
                 continue
@@ -243,10 +238,10 @@ class CookieAutoclicker:
         with open("./save.txt", "r", encoding="utf-8") as file:
             string_to_load = file.read()
 
-        text_area = self.driver.find_element(By.CSS_SELECTOR, "#textareaPrompt")
+        text_area = self.driver.find_element(By.ID, "textareaPrompt")
         text_area.send_keys(string_to_load)
 
-        load_btn = self.driver.find_element(By.CSS_SELECTOR, "#promptOption0")
+        load_btn = self.driver.find_element(By.ID, "promptOption0")
         load_btn.click()
 
         self.options_btn.click()
@@ -255,7 +250,7 @@ class CookieAutoclicker:
         try:
             self.driver.find_element(
                 By.CSS_SELECTOR, ".framed.note.nopic.nodesc"
-            ).find_element(By.CSS_SELECTOR, ".close").click()
+            ).find_element(By.CLASS_NAME, "close").click()
         except NoSuchElementException:
             pass
 
@@ -267,12 +262,12 @@ class CookieAutoclicker:
         export_save_btn.click()
 
         string_to_save = self.driver.find_element(
-            By.CSS_SELECTOR, "#textareaPrompt"
+            By.ID, "textareaPrompt"
         ).get_attribute("value")
         with open("./save.txt", "w", encoding="utf-8") as file:
             file.write(str(string_to_save))
 
-        done_btn = self.driver.find_element(By.CSS_SELECTOR, "#promptOption0")
+        done_btn = self.driver.find_element(By.ID, "promptOption0")
         done_btn.click()
 
         self.options_btn.click()
@@ -283,7 +278,7 @@ class CookieAutoclicker:
 
     def volume_off(self):
         self.options_btn.click()
-        volume_slider = self.driver.find_element(By.CSS_SELECTOR, "#volumeSlider")
+        volume_slider = self.driver.find_element(By.ID, "volumeSlider")
         volume_slider.click()
         volume_slider.send_keys(webdriver.Keys.HOME)
         self.options_btn.click()
@@ -297,10 +292,6 @@ class CookieAutoclicker:
             self.keys_pressed.add(key)
             if all(key in self.keys_pressed for key in SAVE_KEY_COMBINATION):
                 self.save_to_file()
-        if key in CLICK_KEY_COMBINATION:
-            self.keys_pressed.add(key)
-            if all(key in self.keys_pressed for key in CLICK_KEY_COMBINATION):
-                self.big_cookie.click()
 
     def on_release(self, key):
         if key in self.keys_pressed:
